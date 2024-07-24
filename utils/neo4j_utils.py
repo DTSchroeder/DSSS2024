d60c5f9d433137755289e299fae6a64a6cbdd3e6
ramadanomar <ramadan.omaarr@gmail.com>
2024-07-24 15:46:17 +0300

    graph data analytics

--- utils/neo4j_utils.py
+++ utils/neo4j_utils.py
@@ -1,5 +1,7 @@
 from neo4j import GraphDatabase
 import networkx as nx
+import os
+
 
 class Neo4jConnection:
     """
@@ -26,24 +28,26 @@ class Neo4jConnection:
     def __init__(self):
         """ Virtually private constructor. """
         if Neo4jConnection.__instance is not None:
-            raise Exception("This class is a singleton! Use get_instance() instead.")
+            raise Exception(
+                "This class is a singleton! Use get_instance() instead.")
         else:
             Neo4jConnection.__instance = self
 
             # Connection details (Replace with your actual credentials)
             self.uri = "bolt://localhost:7687"
             self.username = "neo4j"
-            self.password = "your_password"
+            self.password = os.environ.get("NEO4J_PASSWORD", "password")
 
             # Establish the connection
-            self.driver = GraphDatabase.driver(self.uri, auth=(self.username, self.password))
+            self.driver = GraphDatabase.driver(
+                self.uri, auth=(self.username, self.password))
 
     def close(self):
         """ Close the database connection. """
         if self.driver:
             self.driver.close()
             Neo4jConnection.__instance = None  # Reset the instance when closing
-            
+
     def run_query(self, query, parameters=None):
         """ Run a Cypher query with optional parameters. """
         with self.driver.session() as session:
@@ -70,7 +74,7 @@ class Neo4jConnection:
         """ Return the count of interactions by book. """
         query = "MATCH ()-[r]->() RETURN r.book as book, count(r) as interaction_count ORDER BY book"
         return self.run_query(query)
-    
+
     def count_interactions(self):
         """ Return the count of interactions. """
         query = "MATCH ()-[r]->() RETURN count(r) as count"
@@ -91,7 +95,7 @@ class Neo4jConnection:
                 "RETURN book, min(num) AS min, max(num) AS max, avg(num) AS avg_characters, stdev(num) AS stdev " \
                 "ORDER BY book"
         return self.run_query(query)
-    
+
     def load_data_into_networkx(self):
         """ Load data from Neo4j into a NetworkX graph. """
         # Query to retrieve all nodes and relationships
@@ -115,22 +119,25 @@ class Neo4jConnection:
 
                 # Add relationships to the graph
                 for rel in record["relationships"]:
-                    graph.add_edge(rel.start_node["name"], rel.end_node["name"], weight=rel["weight"])
+                    graph.add_edge(
+                        rel.start_node["name"], rel.end_node["name"], weight=rel["weight"])
 
             return graph
-        
+
     def print_community_sizes(self, communities):
         """ Print the sizes of the communities. """
         # Calculate the sizes of the detected communities and create a list of (community_index, size) tuples
-        community_sizes = [(idx, len(community)) for idx, community in enumerate(communities)]
+        community_sizes = [(idx, len(community))
+                           for idx, community in enumerate(communities)]
 
         # Sort the list of community sizes by size in descending order
-        sorted_community_sizes = sorted(community_sizes, key=lambda x: x[1], reverse=True)
+        sorted_community_sizes = sorted(
+            community_sizes, key=lambda x: x[1], reverse=True)
 
         print(f"Number of communities: {len(communities)}")
         for idx, size in sorted_community_sizes:
             print(f"Community {idx + 1}: {size} nodes")
-            
+
     def subgraph_largest_community(self):
         """ Return the subgraph of the largest community. """
         # Load the data into a NetworkX graph
@@ -146,8 +153,5 @@ class Neo4jConnection:
         subgraph = graph.subgraph(largest_community)
 
         return subgraph
-         
-    
-    
-    
+
     # TODO ADD SUBGRAPHS
